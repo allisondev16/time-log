c62092753cfdb0381ddf78f9557c49a9c1be6fe6
allisondev16 <allisonmamangun@gmail.com>
2021-07-10 21:12:44 +0800

    Fixed the final time

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -6,35 +6,50 @@ import Box from '@material-ui/core/Box';
 import Zoom from '@material-ui/core/Zoom';
 
 function Time() {
-
-    const [isStart, setStart] = useState(false);
-    const [isBreak, setBreak] = useState(false);
-
-
-    function handleStart() {
-        setStart(true);
-    }
-
-    function handleBreak() {
-        setStart(false);
-        setBreak(true);
-    }
-
     Date.prototype.addHours = function (h) {
         this.setTime(this.getTime() + (h * 60 * 60 * 1000));
         return this;
     }
 
-    const time = new Date();
+    
 
-    const finalTime = time.addHours(10);
+    const [isStart, setStart] = useState(false);
+    const [isBreak, setBreak] = useState(false);
+    const [finalTime, setFinalTime] = useState();
+    // const [breakTime, setBreakTime] = useState();
+
+
+    function handleStart() {
+        setStart(true);
+        const time = new Date();
+
+        const finalTime = time.addHours(10);
+
+        const localFinalTime = finalTime.toLocaleTimeString();
+        setFinalTime(localFinalTime);
+    }
+
+    function handleStartFromBreak() {
+        setStart(true);
+        console.log(finalTime);
+    }
+
+    function handleBreak() {
+        setStart(false);
+        setBreak(true);
+        
+    }
+
+
+
+
 
     return <div>
 
         {isStart && <Fade in={isStart}>
             <div className="time">
                 <h3>Stop working by</h3>
-                <h1>{finalTime.toLocaleTimeString()}</h1>
+                <h1>{finalTime}</h1>
             </div></Fade>}
 
         {isStart && <Zoom in={isStart}>
@@ -50,7 +65,7 @@ function Time() {
             </div></Zoom>}
 
         <div className="startButton">
-            {!isStart && <Button onClick={handleStart} variant="contained" color="primary" size="large">{isBreak ? "Resume Working" : "Start Working"}</Button>}
+            {!isStart && <Button onClick={isBreak ? handleStartFromBreak : handleStart} variant="contained" color="primary" size="large">{isBreak ? "Resume Working" : "Start Working"}</Button>}
         </div>
     </div>
 }
