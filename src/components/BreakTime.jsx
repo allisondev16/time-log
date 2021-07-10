d1dee09fc11ecbe217ba9a21c73fa5828b0c4678
allisondev16 <allisonmamangun@gmail.com>
2021-07-10 19:07:24 +0800

    Additional buttons and created BreakTime component.

---
+++ src/components/BreakTime.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from '@material-ui/core/Button';
+
+function BreakTime(props){
+
+
+
+
+    return <Button onClick={props.onClick} variant="outlined" color="secondary" size="large">Take a Break</Button>
+}
+
+export default BreakTime;
--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from '@material-ui/core/Button';
+import Fade from '@material-ui/core/Fade';
+import BreakTime from "./BreakTime";
+import Box from '@material-ui/core/Box';
+import Zoom from '@material-ui/core/Zoom';
 
-function Time(){
+function Time() {
 
-    
+    const [isStart, setStart] = useState(false);
+    const [isBreak, setBreak] = useState(false);
 
-    Date.prototype.addHours = function(h) {
-        this.setTime(this.getTime() + (h*60*60*1000));
+
+    function handleStart() {
+        setStart(true);
+    }
+
+    function handleBreak() {
+        setStart(false);
+        setBreak(true);
+    }
+
+    Date.prototype.addHours = function (h) {
+        this.setTime(this.getTime() + (h * 60 * 60 * 1000));
         return this;
     }
 
     const time = new Date();
-    
+
     const finalTime = time.addHours(10);
 
-    return <div className="time">
-        <h3>Stop working by</h3>
-        <h1>{finalTime.toLocaleTimeString()}</h1>
-        <Button variant="contained" color="primary" size="large">Start Working</Button>
+    return <div>
+
+        {isStart && <Fade in={isStart}>
+            <div className="time">
+                <h3>Stop working by</h3>
+                <h1>{finalTime.toLocaleTimeString()}</h1>
+            </div></Fade>}
+
+        {isStart && <Zoom in={isStart}>
+            <div className="stopButton">
+                <Box ml={2} mr={2} mt={10}>
+                    <BreakTime
+                        onClick={handleBreak}
+                    />
+                </Box>
+                <Box ml={2} mr={2} mt={10}>
+                    <Button variant="contained" color="secondary" size="large">Done Working</Button>
+                </Box>
+            </div></Zoom>}
+
+        <div className="startButton">
+            {!isStart && <Button onClick={handleStart} variant="contained" color="primary" size="large">{isBreak ? "Resume Working" : "Start Working"}</Button>}
+        </div>
     </div>
 }
 
