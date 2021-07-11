78a7e49e0dc32ebee8f52e1a3a4131c536966384
allisondev16 <allisonmamangun@gmail.com>
2021-07-11 16:28:08 +0800

    Added functionality to adjust the final time as per the break time

--- src/components/BreakTime.jsx
+++ src/components/BreakTime.jsx
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 
 function BreakTime(props){
 
-
+    
 
 
     return <Button onClick={props.onClick} variant="outlined" color="secondary" size="large">Take a Break</Button>
--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -12,51 +12,54 @@ function Time() {
     }
 
     
-    
 
     const [isStart, setStart] = useState(false);
-    const [isBreak, setBreak] = useState(false);
     const [finalTime, setFinalTime] = useState();
+    
+    const [isBreak, setBreak] = useState(false);
     const [breakTime, setBreakTime] = useState();
+
     const [resumeTime, setResumeTime] = useState();
+
 
     function handleStart() {
         setStart(true);
-        const time = new Date();
-        const finalTime = time.addHours(10);
-        const localFinalTime = finalTime.toLocaleTimeString();
-        setFinalTime(localFinalTime);
+        // let time = new Date();
+        let finalTime = new Date().addHours(10);
+        setFinalTime(finalTime);
+    }
+
+    function handleBreak() {
+        setStart(false);
+        setBreak(true);
+        setBreakTime(new Date());
+        console.log("Resume time: "+resumeTime);
     }
 
     function handleStartFromBreak() {
         setStart(true);
         console.log("Final time: "+finalTime);
         console.log("Break time: "+breakTime);
-        const resumeTime = new Date();
-        const localResumeTime = resumeTime.toLocaleTimeString();
-        setResumeTime(localResumeTime);
+        let resumeTime = new Date();
+        setResumeTime(resumeTime);
         
+        let breakDuration = resumeTime - breakTime;
+        let breakDurationSeconds = breakDuration/1000;
+        console.log("Break duration in seconds: " + breakDurationSeconds);
+
+        let newFinalTime = new Date(finalTime.getTime() + breakDuration);
+        console.log("New Final Time :" + newFinalTime);
+        setFinalTime(newFinalTime);
     }
 
-    function handleBreak() {
-        setStart(false);
-        setBreak(true);
-        const breakTime = new Date();
-        const localBreakTime = breakTime.toLocaleTimeString();
-        setBreakTime(localBreakTime);
-        console.log("Resume time: "+resumeTime);
-    }
-
-
-
-
+    
 
     return <div>
 
         {isStart && <Fade in={isStart}>
             <div className="time">
                 <h3>Stop working by</h3>
-                <h1>{finalTime}</h1>
+                <h1>{finalTime.toLocaleTimeString()}</h1>
             </div></Fade>}
 
         {isStart && <Zoom in={isStart}>
