738216f75550f20c191acec075f53d00501408fa
allisondev16 <allisonmamangun@gmail.com>
2021-08-19 21:48:44 +0800

    react-create-app on Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,24 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const Cors = require("cors");
 const path = require('path');
-
-// App Config
 const app = express();
-const port = process.env.PORT || 5000;
-const connection_url = "mongodb+srv://timelog:f@E7D_6prRXdma3@cluster0.9fgna.mongodb.net/timelogDB?retryWrites=true&w=majority";
+require('./database');
 
 // Middlewares
 app.use(express.json());
 app.use(Cors());
 
-// DB config
-mongoose.connect(connection_url, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-});
-
 // API
 const timeLogs = require('./api/timeLogs');
-app.use('/api/timeLogs', timeLogs);
+app.use('/time-log/data', timeLogs);
 
-// API Endpoints
-// app.use(express.static(path.join(__dirname, '../build')));
-// app.get('/home', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../build'))
-// });
+//API Endpoints
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
 
-app.listen(port, function () {
-  console.log("Server is up!")
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
--- src/components/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL: 'http://time-log-16-server.herokuapp.com/'
-});
-
-export default instance;
