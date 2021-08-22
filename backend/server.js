1be6ac13d967dcc656eda990b10904f989907df0
allisondev16 <allisonmamangun@gmail.com>
2021-08-22 14:20:43 +0800

    react-create-app on Heroku

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Cors = require("cors");
 const path = require("path");
 const app = express();
-require('./database');
+require("./database");
 
 // Middlewares
 app.use(express.json());
