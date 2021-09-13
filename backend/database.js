17c58ec892916094e98a022a63ad26e4374354b9
allisondev16 <allisonmamangun@gmail.com>
2021-09-13 22:28:20 +0800

    Added dotenv

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const connection = "mongodb+srv://timelog:f@E7D_6prRXdma3@cluster0.9fgna.mongodb.net/timelogDB2?retryWrites=true&w=majority";
+require('dotenv').config()
+const connection = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9fgna.mongodb.net/timelogDB2?retryWrites=true&w=majority`;
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
